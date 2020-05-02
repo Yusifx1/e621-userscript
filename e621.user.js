ebb9a3f77a3ee47cab910d57d6d6e57e72e7768a
Yusifx1 <63213937+Yusifx1@users.noreply.github.com>
2020-05-02 15:46:49 +0400

    V0.9

--- e621.user.js
+++ e621.user.js
@@ -1,56 +1,83 @@
 // ==UserScript==
 // @name         e621
-// @version      0.8
+// @version      0.9
 // @homepageURL https://github.com/Yusifx1/e621-userscript/
 // @downloadURL        https://github.com/Yusifx1/e621-userscript/releases/latest/download/e621.user.js
 // @description  e621 pool and etc. downloader and parser
 // @author        Yusifx1
-// @match        https://e621.net/pools*
+// @match        https://e621.net/*
+// @exclude   https://e621.net/pools
 // @grant        GM_download
 // ==/UserScript==
+
+var cpu = window.location.href,
+loc= window.location.pathname,
+body = document.querySelector ( '#top' ),
+ids,name,url,span,json,p,result,xhr;
+if ( cpu.match(/^.*.e621.net\/pools\/*/) ) {
 var button = document.createElement("button");
 button.innerHTML = "Download Pool";
-var body = document.querySelector ( '#top' );
 body.appendChild(button);
-button.addEventListener ("click", function() {
-var pool= window.location.pathname ;
-var url="https://e621.net" + pool + ".json";
-var xhr = new XMLHttpRequest();
+button.style = 'background-color: #00549e;color:white;font-size: 18px;border-radius: 8px;';
+button.addEventListener ("click", pool )
+onepage() 
+}  
+
+if ( window.location.pathname == "/posts" ) { 
+p=1
+
+onepage() 
+}
+
+function pool() {
+if (window.confirm("Download this pool?")) { 
+  getid()
+  dpool()
+}
+}
+
+
+function getid() {
+url="https://e621.net" + loc + ".json";
+xhr = new XMLHttpRequest();
 xhr.open('GET', url, false);
 xhr.send();
-var rjson=xhr.responseText ;
-var json=JSON.parse(rjson);
-var name= json.name;
-var ids= json.post_ids;
-alert(name);
+json=xhr.responseText ;
+json=JSON.parse(json);
+name= json.name;
+ids= json.post_ids;
+}
+
+function dpool() {
 var i =1;
 for  (var pid in ids) {
-var purl = "https://e621.net/posts/" + ids[pid] + ".json";
-var pxhr = new XMLHttpRequest();
-pxhr.open('GET', purl, false);
-pxhr.send();
-var rpjson=pxhr.responseText ;
-var pjson=JSON.parse(rpjson);
-var ext= "." + pjson.post.file.ext;
-var durl= pjson.post.file.url;
+url = "https://e621.net/posts/" + ids[pid] + ".json";
+xhr = new XMLHttpRequest();
+xhr.open('GET', url, false);
+xhr.send();
+json=xhr.responseText ;
+json=JSON.parse(json);
+var ext= "." + json.post.file.ext;
+var durl= json.post.file.url;
 var count = i + '';     while (count.length < 3) {         count = '0' + count;     }   
 name = name.replace(/[*/<>?\\|&]/g, s =>
   String.fromCharCode(s.charCodeAt(0) + 0xFF00 - 0x20));
-var path= name
+var path= name;
 GM_download(durl, path+ "/" + count + ext);
 i++;
 }
-});
-var button2 = document.createElement("button");
-button2.innerHTML = "Get post links";
-body.appendChild(button2);
-button2.addEventListener ("click", function() {
+}
 
+
+function onepage() {
+var button2 = document.createElement("button");
+button2.innerHTML = "Get page post links";
+body.appendChild(button2);
+button2.style = 'background-color: #00549e;color:white;font-size: 18px;border-radius: 8px;';
+button2.addEventListener ("click", function() {
 var	post = document.querySelectorAll ( '.post-preview' ),
 span = document.createElement ( 'span' ),
     x,src;
-
-
 
 if ( post != null ) {
 	for ( x = 0; x < post.length; x++ ) {
@@ -58,8 +85,10 @@ if ( post != null ) {
 
 		span.innerHTML += '<a href = "' + src + '">' + src + '</a><br>';
 	}
-	span.style = 'display: inline-block; column-gap: 5px; column-count: 3; max-height: 200px; overflow: auto;';
+	span.style = 'background-color:#102545;display: block; font-size: 12px;border-radius: 8px;column-count:2;  max-height: 150px; overflow: auto;';
 	body.appendChild ( span );
 }
   
   });
+}
+ 
